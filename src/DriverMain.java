952b45531098f7128ee5896af0f121cd99e0401b
Samit Sandhu <ssand139@my.centennialcollege.ca>
2025-09-10 14:05:54 -0400

    LtoG

--- src/DriverMain.java
+++ src/DriverMain.java
@@ -4,12 +4,18 @@ public class DriverMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		GradeCalculation gradeCalc = new GradeCalculation();
-		
+		MilesToKm milesConvert = new MilesToKm();
+		LtreToGal litresConvert = new LtreToGal();
 		
 		//Question 1
 		gradeCalc.GradeCalc();
 		
 		//Question 2
+		milesConvert.Convert();
+		
+		//Question 3
+		litresConvert.Convert();
+		
 		
 
 	}
--- src/LtreToGal.java
+++ src/LtreToGal.java
@@ -1,4 +1,16 @@
-
+import java.util.Scanner;
 public class LtreToGal {
 
+	Scanner sc = new Scanner(System.in);
+	
+	public void Convert()
+	{
+		double litres;
+		
+		System.out.println("Enter Litres to convert to gallons: ");
+		litres = sc.nextDouble();
+		double gallons = litres / 3.785;
+		
+		System.out.printf("%f Litres = %f US Liquid Gallons", litres, gallons);
+	}
 }
